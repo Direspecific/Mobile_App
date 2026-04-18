7dab5ef2bbf9520668d91f0af39687316626f3b5
Nabizen <baltarneiladrian@gmail.com>
2026-04-18 18:40:07 +0800

    Added extra dependencies for FrameOutput

--- app/index.tsx
+++ app/index.tsx
@@ -4,11 +4,19 @@ import {
   Camera,
   useCameraDevice,
   useCameraPermission,
+  useFrameOutput,
 } from 'react-native-vision-camera'
 
 export default function Index() {
   const { hasPermission, requestPermission } = useCameraPermission()
   const device = useCameraDevice('back')
+  const frameOutput = useFrameOutput({
+    onFrame(frame) {
+      'worklet'
+      console.log(`Received ${frame.width}x${frame.height} Frame!`)
+      frame.dispose()
+    }
+  })
 
   useEffect(() => {
     const run = async () => {
@@ -33,6 +41,7 @@ export default function Index() {
       style={{ flex: 1 }}
       device={device}
       isActive={true}
+      outputs={[frameOutput]}
     />
   )
 }
