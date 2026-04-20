318b952ac2760e018de41ed3e83694194e03e539
Nabizen <baltarneiladrian@gmail.com>
2026-04-20 15:03:51 +0800

    Added a frame resizer to make it input compatible with model

--- app/index.tsx
+++ app/index.tsx
@@ -9,36 +9,52 @@ import {
 
 import { useTensorflowModel } from 'react-native-fast-tflite'
 import { useResizer } from 'react-native-vision-camera-resizer'
-import { runOnJS } from 'react-native-reanimated'
 
 export default function Index() {
   const objectDetection = useTensorflowModel(require('./models/blaze_face_short_range.tflite'), [])
   const model = objectDetection.state === 'loaded' ? objectDetection.model : undefined
-  // const { resize } = useResizePlugin()
 
   const { hasPermission, requestPermission } = useCameraPermission()
-  const device = useCameraDevice('back')
+  const device = useCameraDevice('front')
 
+  // When trying to run this app, run this on a physical devices instead of an emulator
+  // The resizer returns as undefined since Resizer pipeline requires Vulkan extensions (VK_ANDROID_external_memory_android_hardware_buffer).
+  // Which is only available on Android 8.0+. Android emulators frequently don't support these extensions
+  // Therefore making the frameOutput not run properly
   const { resizer } = useResizer({
     width: 128,
     height: 128,
     channelOrder: 'rgb',
     dataType: 'float32',
-    pixelLayout: 'planar',
+    pixelLayout: 'interleaved',
     scaleMode: 'cover', // contain or 'cover'
   })
-  if (!resizer) return
 
   const frameOutput = useFrameOutput({
     pixelFormat: 'yuv',
+    enablePhysicalBufferRotation: false,
     onFrame(frame) {
       'worklet'
+
+      if (!resizer || !model) {
+        if (!resizer && !model) {
+          console.log("Resizer and model are undefined")
+        } else if (!resizer) {
+          console.log("Resizer is undefined")
+        } else {
+          console.log("Model is not loaded yet")
+        }
+
+        frame.dispose()
+        return
+      }
 
       const resized = resizer.resize(frame)
       const pixels = resized.getPixelBuffer()
 
-      console.log(resized.width, resized.height)
-      // console.log(`Received ${frame.width}x${frame.height} Frame!`)
+      const outputs = model.runSync([pixels])
+
+
       resized.dispose()
       frame.dispose()
     }
