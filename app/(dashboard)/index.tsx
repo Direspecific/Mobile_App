a4bb07f2686001123269c79c60ae340804033379
Nabizen <baltarneiladrian@gmail.com>
2026-04-27 18:01:38 +0800

    Added Status Card Component and Semi Done Home Tab

--- app/(dashboard)/index.tsx
+++ app/(dashboard)/index.tsx
@@ -1,12 +1,52 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, ScrollView  } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import StatusCard from "./components/StatusCard";
 
 export default function Home() {
-  return (
-    <View className="flex-1 justify-center items-center">
-      <View className="flex-1 justify-center items-center">
-        <Text>Home</Text>
-      </View>
+  const logo = require("../../assets/images/favicon.png");
 
-    </View>
+  return (
+    <ScrollView className="flex-1">
+      <SafeAreaView className="flex-row items-center justify-between" edges={['top']}>
+        <View className="p-4">
+          <Text className="text-lg font-bold text-blue-500">
+            Welcome to TaSKReg!
+          </Text>
+          <Text className="text-sm text-gray-500">
+            C8nnect IT Solutions
+          </Text>
+        </View>
+        <Image source={logo} className="w-12 h-12 rounded-full mr-6 border"/>
+      </SafeAreaView>
+      
+      <StatusCard
+        logo={logo}
+        label="Registration Status"
+        footer="Registration open Until April 25, 2026"
+      />
+
+      <Text className="pl-4 text-lg font-bold">
+        Your Voting Location
+      </Text>
+
+      <StatusCard
+        logo={logo}
+        label="Registration Status"
+        footer="Registration open Until April 25, 2026"
+      />
+
+      <Text className="pl-4 text-lg font-bold">
+        Upcoming Election
+      </Text>
+
+      <StatusCard
+        logo={logo}
+        status="Primary Election"
+        label=""
+        footer="Registration open Until April 25, 2026"
+        variant="pending"
+      />
+
+    </ScrollView>
   );
 }
