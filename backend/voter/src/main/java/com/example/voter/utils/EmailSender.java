03ff31bdf615355cf11b31ac6719f219fc44510f
Andrew Laurence T. Fat <111488211+Direspecific@users.noreply.github.com>
2026-05-11 23:30:41 +0800

    Delete backend/voter directory

--- backend/voter/src/main/java/com/example/voter/utils/EmailSender.java
+++
@@ -1,8 +0,0 @@
-package com.example.voter.utils;
-
-public class EmailSender {
-
-    public static void send(String to, String otp) {
-        System.out.println("Sending OTP " + otp + " to " + to);
-    }
-}
